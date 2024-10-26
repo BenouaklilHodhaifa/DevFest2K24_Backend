84f1a139c733030913411e4de0b76c67ac2f713c
HodhaifaBenouaklil <kh_benouaklil@esi.dz>
2024-10-26 13:35:42 +0100

    fix

--- main/ai_models/forecast/prediction.py
+++ main/ai_models/forecast/prediction.py
@@ -19,6 +19,7 @@ def forecast(kpi_name, nb_steps, data):
 		forecaster = pickle.load(file)
 
 	df = pd.DataFrame(data)
+	df.drop_duplicates(subset=['timestamp'], inplace=True)
 	df["Timestamp"] = df["timestamp"]
 	df["KPI_Value"] = df["kpi_value"]
 	df.drop(columns = ["timestamp", "kpi_value"])
