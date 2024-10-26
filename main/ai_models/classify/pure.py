25ef91ecaf2bf51e55d6e2d9e66632d1f100b380
HodhaifaBenouaklil <kh_benouaklil@esi.dz>
2024-10-26 13:59:29 +0100

    fix

--- main/ai_models/classify/pure.py
+++ main/ai_models/classify/pure.py
@@ -10,7 +10,9 @@ BASE_DIR = Path(__file__).resolve().parent
 def predict(data: list[dict], kpi: str):
     test_data = pd.DataFrame(data)
     test_data['Timestamp'] = pd.to_datetime(test_data['Timestamp'])
-    test_data = test_data[~test_data.index.duplicated(keep='first')]
+    # test_data = test_data[~test_data.index.duplicated(keep='first')]
+    # remove duplicate timestamps
+    test_data = test_data.drop_duplicates(subset='Timestamp', keep='first')
 
     test_data.set_index('Timestamp', inplace=True)
     test_data.sort_index(inplace=True)
